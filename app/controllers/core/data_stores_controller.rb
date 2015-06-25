e2a4a458713e25b9762c48c473d6e18439b7effc
Ritvvij Parrikh (Pykih Staff) <ritvij.j@gmail.com>
2015-06-25 13:46:03 +0530

    started removing data store

--- app/controllers/core/data_stores_controller.rb
+++ app/controllers/core/data_stores_controller.rb
@@ -1,8 +1,8 @@
 class Core::DataStoresController < ApplicationController
 
   before_action :sudo_project_member!, only: [:new, :upload, :update, :destroy]
-  before_action :sudo_public!, only: [:index, :show, :index_all, :csv]
-  before_action :set_token, only: [:show, :destroy,:csv]
+  before_action :sudo_public!, only: [:index, :show, :index_all]
+  before_action :set_token, only: [:show, :destroy]
  #------------------------------------------------------------------------------------------------------------------
   # CRUD
 
@@ -39,38 +39,6 @@ class Core::DataStoresController < ApplicationController
       end
     end
   end
-
-  def destroy
-    is_deletable = @data_store.check_dependent_destroy?
-    if is_deletable[:is_deletable]
-      begin
-        Nestful.post REST_API_ENDPOINT + "#{@account.slug}/#{@core_project.slug}/#{@data_store.slug}/grid/delete", {:token => @alknfalkfnalkfnadlfkna}, :format => :json
-        if @data_store.cdn_published_url.present? #PUBLISH TO CDN Functionality
-          @data_store.update_attributes(marked_to_be_deleted: "true")
-          Core::S3File::DestroyWorker.perform_async("DataStore", @data_store.id)
-          notice = t("d.m")
-        else
-          @data_store.destroy
-          notice = t("d.s")
-        end
-        redirect_to _account_project_path(@core_project.account, @core_project), notice: notice
-      rescue => e
-        redirect_to _account_project_path(@core_project.account, @core_project), alert: e.to_s
-      end
-    else
-      redirect_to _account_project_path(@core_project.account, @core_project,d: is_deletable[:dependent_to_destroy],d_id: @data_store.id)
-    end
-  end
-
-  #------------------------------------------------------------------------------------------------------------------
-  # OTHER
-
-  def csv
-    s = "/tmp/#{SecureRandom.hex(24)}.csv"
-    @data_store.generate_file_in_tmp(s,@alknfalkfnalkfnadlfkna)
-    send_data IO.read(s), :type => "application/vnd.ms-excel", :filename => "#{@data_store.slug}.csv", :stream => false
-    File.delete(s)
-  end
   #------------------------------------------------------------------------------------------------------------------
 
   private
@@ -81,11 +49,6 @@ class Core::DataStoresController < ApplicationController
   end
 
   def set_data_store
-    if params[:data_id].present?
-      @data_store = @core_project.data_stores.friendly.find(params[:data_id])
-    else
-      @data_store = @core_project.data_stores.friendly.find(params[:id])
-    end
     @core_projects = current_account.core_projects.where("id <> ?", @data_store.core_project_id)  if current_account.present?
     @parent = @data_store.clone_parent  if @data_store.clone_parent_id.present?
   end
