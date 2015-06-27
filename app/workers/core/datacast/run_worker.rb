4860964b9e92e39d42e5198b3fe7cb925edb1780
Ritvvij Parrikh (Pykih Staff) <ritvij.j@gmail.com>
2015-06-27 12:44:24 +0530

    bugs in worker

--- app/workers/core/datacast/run_worker.rb
+++ app/workers/core/datacast/run_worker.rb
@@ -15,18 +15,13 @@ class Core::Datacast::RunWorker
         time_taken = Time.now - start_time
         if (prev.output.present? and prev.fingerprint != fingerprint) or prev.output.blank?  # IS DATA CHANGE
           d.average_execution_time = ((d.average_execution_time * d.count_of_queries) + time_taken)/d.count_of_queries
-          d.last_data_changed_at = Time.now
-          d.number_of_rows = response["number_of_rows"]
-          d.number_of_columns = response["number_of_columns"]
-          d.size = (response["query_output"].bytesize.to_f)/1024
+          d.last_data_changed_at   = Time.now
+          d.number_of_rows         = response["number_of_rows"]
+          d.number_of_columns      = response["number_of_columns"]
+          d.size                   = (response["query_output"].bytesize.to_f)/1024
+          d.error                  = ""
           prev.update_attributes(output: response["query_output"], fingerprint: fingerprint)
-          d.error = ""
         end
-        #------------------------------------------------------------------------------------- REPEAT QUERY
-        #if !core_viz.refresh_freq_in_minutes.nil? and core_viz.refresh_freq_in_minutes > 0
-          #ref_min = core_viz.refresh_freq_in_minutes * 60
-          #Core::Viz::DatagramWorker.perform_at((Time.now + ref_min), core_viz.id)
-        #-------------------------------------------------------------------------------------
       else
         d.error = response["query_output"]
       end
@@ -41,6 +36,10 @@ class Core::Datacast::RunWorker
     d.count_of_queries = d.count_of_queries + 1
     d.last_run_at = Time.now
     d.save
+    #REPEAT QUERY
+    if d.refresh_frequency.present? and d.refresh_frequency > 0
+      Core::Datacast::RunWorker.perform_at((Time.now + (d.refresh_frequency * 60)), core_datacast_id)
+    end
   end
   
 end
