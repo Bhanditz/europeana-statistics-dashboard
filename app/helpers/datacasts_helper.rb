7624ea332aa673b095d36ae5dffb80839a5d9733
Ritvvij Parrikh (Pykih Staff) <ritvij.j@gmail.com>
2015-06-29 10:14:31 +0530

    signal lights

--- app/helpers/datacasts_helper.rb
+++ app/helpers/datacasts_helper.rb
@@ -1,7 +1,7 @@
 module DatacastsHelper
   
   def signal_datacast_status(d)
-    return d.error.present? ? "signal/red.png" : d.last_run_at.blank? ? "signal/yellow.png" : "signal/green.png"
+    d.error.present? ? "<div class='signal sig-red'></div>".html_safe : d.last_run_at.blank? ? "<div class='signal sig-gray'></div>".html_safe : "<div class='signal sig-green'></div>".html_safe
   end
-  
+    
 end
