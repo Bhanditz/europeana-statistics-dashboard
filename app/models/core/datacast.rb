4d5cf7dc3016836fcf3ea191fff80af0266a4c35
amitbadheka <ab@pykih.com>
2015-06-29 20:39:32 +0530

    [#97878926] column_meta for core_datacast

--- app/models/core/datacast.rb
+++ app/models/core/datacast.rb
@@ -61,7 +61,44 @@ class Core::Datacast < ActiveRecord::Base
     end
     a
   end
-  
+
+  def self.get_data_distribution(data)
+    #Doubt: What about JSON/HSTORE datatypes
+    datatype_distribution = {}
+    column_names = data.shift
+    column_names.each do |col|
+      datatype_distribution[col] = {"string" => 0, "boolean" => 0, "float" => 0, "integer" => 0, "date" => 0, "blank" => 0}
+    end
+    data.each do |row|
+      row.each_with_index do |elem, index|
+        datatype = Core::Datacast.get_element_datatype(elem)
+        datatype_distribution[column_names[index]][datatype] += 1
+      end
+    end
+    return datatype_distribution
+  end
+
+  def self.get_col_datatype(col_data_distribution)
+    # Need to work on it. This is scrappy's code, for reference
+    # possible_types = datatype_distribution.keys
+    # return "double" if datatype_distribution.has_key?("double") and (possible_types & ["date", "boolean"]).length < 1 and datatype_distribution["double"] > 0
+    # return "integer" if datatype_distribution.has_key?("integer") and (possible_types & ["date", "boolean", "double"]).length < 1 and datatype_distribution["integer"] > 0
+    # return "boolean" if datatype_distribution.has_key?("boolean") and (possible_types & ["date", "double"]).length < 1 and datatype_distribution["boolean"] > 0
+    # return "date" if datatype_distribution.has_key?("date") and (possible_types & ["boolean", "double", "integer"]).length < 1 and datatype_distribution["date"] > 0
+    # return "string" if datatype_distribution.has_key?("string") and datatype_distribution["string"] > 0
+    return "string" #else - worst case scenario
+  end
+
+  def self.get_element_datatype(element)
+    return "blank" if element.blank?
+    element.strip!
+    return "boolean" if ['t', 'true', 'f', 'false', 'yes', 'no', 'y', 'n'].include?(element.downcase)
+    return "date" if (element.match(/^[0-3]?[0-9](-|\/)[0-1]?[0-9](-|\/)[0-9]{2,4}$/) or element.match(/^[0-1]?[0-9](-|\/)[0-3]?[0-9](-|\/)[0-9]{2,4}$/) or element.match(/^[0-3]?[0-9](-|\/)[0-1]?[0-9](-|\/)[0-9]{2,4}( |)(\s|\d(|:|\d)+)$/) or element.match(/^[0-1]?[0-9](-|\/)[0-3]?[0-9](-|\/)[0-9]{2,4}( |)(\s|\d(|:|\d)+)$/)) and Date.parse(element).present?
+    return "float" if element.match(/^[-+]?[0-9]*\.[0-9]+$/)
+    return "integer" if  element.match(/^[-+]?[0-9]+$/)
+    return "string"
+  end
+
   def run
     Core::Adapters::Db.run(self.core_db_connection, self.query, self.format)
   end
