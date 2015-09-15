360070d52eaa0cc2e280b73f301836deab7a557c
amitbadheka <ab@pykih.com>
2015-09-16 00:29:20 +0530

    Change unicorn pid path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/var/www/html/rumi-io"
-pid "/var/www/html/rumi-io/tmp/pids/unicorn.pid" 
-stderr_path "/var/www/html/rumi-io/log/unicorn.log" 
-stdout_path "/var/www/html/rumi-io/log/unicorn.log" 
+working_directory "/var/www/html/datastory"
+pid "/var/www/html/datastory/tmp/pids/unicorn.pid" 
+stderr_path "/var/www/html/datastory/log/unicorn.log" 
+stdout_path "/var/www/html/datastory/log/unicorn.log" 
 listen "/tmp/unicorn.sock"
 worker_processes 2 
 timeout 300
