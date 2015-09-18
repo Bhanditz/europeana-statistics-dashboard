21e0116898417cf7b290e36d8b67d8e11fac1672
amitbadheka <ab@pykih.com>
2015-09-18 16:43:25 +0530

    Redirect user to sign in page if not signed in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,8 @@ class StaticPagesController < ApplicationController
     if account_signed_in?
       p = Core::Project.first
       redirect_to _account_project_path(p.account, p)
+    else
+      redirect_to new_account_session_path
     end
   end
   
