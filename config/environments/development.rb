9167dddb6dd6c04c597f646b265ed0f5deb9354e
amitbadheka <ab@pykih.com>
2015-10-14 13:51:17 +0530

    #1 Moves every configuration key to env variables

---
+++ config/environments/development.rb
@@ -0,0 +1,61 @@
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+  config.active_record.raise_in_transactional_callbacks = true
+
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
+  config.assets.precompile += ['accounts.js', 'core_projects.js', 
+                               'core_themes.js', 'data_stores.js', 'vizs.js',"datacast.js", "articles.js", "reports.js", "europeana.css"]
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
+  
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    
+  BASE_URL = "http://127.0.0.1:3000"
+  
+  #redis_client = Redis.new(:url => "redis://user:PASSWORD@datahub-development.wdzjlu.0001.usw2.cache.amazonaws.com")
+  #Resque.redis = redis_client
+  
+  REST_API_ENDPOINT = "http://localhost:9292/v1/"
+  
+  
+  #GA IDS
+  GA_CLIENT_ID=ENV["GA_CLIENT_ID"]
+  GA_CLIENT_SECRET=ENV["GA_CLIENT_SECRET"]
+  GA_SCOPE=ENV["GA_SCOPE"]
+  GA_REFRESH_TOKEN=ENV["GA_REFRESH_TOKEN"]
+  GA_IDS=ENV["GA_IDS"]
+
+  REDIS_HOST = ENV["REDIS_HOST"]
+  REDIS_PORT = ENV["REDIS_PORT"]
+ 
+end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,33 @@
+class Constants
+  
+  #Accounts
+  ACC_U = "User" #Constants::ACC_U
+  
+  #Regex
+  EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i  #Constants::EMAIL
+  PASSWORD = /^(?=.*\d)(?=.*([a-z]|[A-Z]))([\x20-\x7E]){8,}$/  #Constants::PASSWORD
+  GIT_URL = /(^git@)(.+)(:)(.+)(\/)(.+)(.git$)/ #Constants::GIT_URL
+  URL = URI::regexp(%w(http https))  #Constants::URL
+  
+  #SUDO
+  #Owner, Collaborator, Viewer
+  SUDO_111 = [true, true, true]
+  SUDO_011 = [false, true, true]
+  SUDO_001 = [false, false, true]
+  
+  #Permissions
+  ROLE_C = "Collaborator" #Constants::ROLE_C
+  ROLE_O = "Owner" #Constants::ROLE_O
+  STATUS_A = "Accepted" #Constants::STATUS_A
+  STATUS_I = "Invited" #Constants::STATUS_I
+  
+  #VERSION
+  RUMI_VERSION = "1.0"
+    
+  #Constants::STUPID_PASSWORDS
+  STUPID_PASSWORDS = ["password", "123456", "12345678", "abc123", "qwerty", "monkey", "letmein", "dragon", "111111", "baseball", "iloveyou", "trustno1", "1234567", "sunshine", "master", "123123", "welcome", "shadow", "ashley", "football", "jesus", "michael", "ninja", "mustang", "password1", "123456789", "admin", "princess", "azerty", "photoshop", "000000", "1234567890"]
+  
+  #Constants::STUPID_USERNAMES
+  STUPID_USERNAMES = ["about","account","add","admin","api","app","apps","archive","archives","auth","better","billing","blog","cache","cdn","changelog","codereview","compare","config","connect","contact","create","delete","dev","direct_messages","downloads","edit","email","enterprise","faq","favorites","feed","feeds","follow","followers","following","ftp","gist","help","home","hosting","imap","invitations","invite","jobs","lists","log","login","logout","logs","mail","map","maps","master","messages","mine","news","next","oauth","oauth_clients","openid","organizations","plans","pop","popular","privacy","production","projects","register","remove","replies","rss","rsync","save","search","security","sessions","settings","sftp","shop","signup","sitemap","ssh","ssl","staging","status","stories","styleguide","subscribe","support","terms","tour","translations","trends","unfollow","unsubscribe","url","user","widget","widgets","wiki","www","xfn","xmpp", "404","account","accounts","admin","admins","administrator","administrators","billing","block","blog","bot","bots","bug","bugs","calendar","careers","chat","checkuser","client","clients","comment","comments","contract","contracts","create","customer","customers","customersupport","dev","development","download","downloads","ecommerce","enquiries","error","errors","event","events","faq","feedback","flock","form","forms","forum","github","help","herd","home","info","internal","invoice","invoices","issue","issues","job","jobs","log","mail","manual","new","news","office","press","print","recruit","script","scripts","service","services","shepherd","shepherdess","staff","staging","support","sysop","team","training","trainings","troll","version","video","videos","webmail","wiki","test","testing","tester","beta","mobile","list","site","friend","member"]
+
+end
