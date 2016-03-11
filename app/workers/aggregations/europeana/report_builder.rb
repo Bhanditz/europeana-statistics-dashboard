527a8736860139ae85952a9bb875d31a4b03c9e9
Aashutosh Bhatt <aashutosh.bhatt@pykih.com>
2016-03-11 17:07:45 +0530

    Removed comments, added precompiled assets.

--- app/workers/aggregations/europeana/report_builder.rb
+++ app/workers/aggregations/europeana/report_builder.rb
@@ -21,8 +21,6 @@ class Aggregations::Europeana::ReportBuilder
     core_template = Core::Template.default_europeana_template
     required_variables = core_template.required_variables['required_variables']
     core_vizs = aggregation.core_vizs
-    #["main_chart","topcountries","total_items","open_for_reuse"]
-    #Aashutosh change from here
 
     #"$main_chart$"
     line_chart_content_key = required_variables.shift
@@ -35,18 +33,6 @@ class Aggregations::Europeana::ReportBuilder
     top_countries_content_value = core_vizs.top_country.first.auto_html_json
     top_countries_content_value["title"] = "Top 25 Countries"
     variable_object[top_countries_content_key] = top_countries_content_value
-
-    # unless aggregation.genre == "data_provider"
-    #   #"$TOTAL_PROVIDERS_COUNT$"
-    #   provider_content_key = required_variables.shift
-    #   providers_content_value = core_vizs.provider_counts.first.auto_html_div
-    #   variable_object[provider_content_key] = providers_content_value
-    # end
-
-    # #"$TOP_DIGITAL_OBJECTS$"
-    # top_digital_objects_content_key = required_variables.shift
-    # top_digital_objects_content_value = aggregation.top_digital_objects_auto_html
-    # variable_object[top_digital_objects_content_key] = top_digital_objects_content_value
 
     #"$total_items$"
     media_type_key = required_variables.shift
--- app/workers/impl/data_providers/report_builder.rb
+++ app/workers/impl/data_providers/report_builder.rb
@@ -22,8 +22,6 @@ class Impl::DataProviders::ReportBuilder
     core_template = Core::Template.default_europeana_template
     required_variables = core_template.required_variables['required_variables']
     core_vizs = aggregation.core_vizs
-    #["main_chart","topcountries","total_items","open_for_reuse"]
-    #Aashutosh change from here
 
     #"$main_chart$"
     line_chart_content_key = required_variables.shift
@@ -37,18 +35,6 @@ class Impl::DataProviders::ReportBuilder
     top_countries_content_value["title"] = "Top 25 Countries"
     variable_object[top_countries_content_key] = top_countries_content_value
 
-    # unless aggregation.genre == "data_provider"
-    #   #"$TOTAL_PROVIDERS_COUNT$"
-    #   provider_content_key = required_variables.shift
-    #   providers_content_value = core_vizs.provider_counts.first.auto_html_div
-    #   variable_object[provider_content_key] = providers_content_value
-    # end
-
-    # #"$TOP_DIGITAL_OBJECTS$"
-    # top_digital_objects_content_key = required_variables.shift
-    # top_digital_objects_content_value = aggregation.top_digital_objects_auto_html
-    # variable_object[top_digital_objects_content_key] = top_digital_objects_content_value
-
     #"$total_items$"
     media_type_key = required_variables.shift
     media_type_value = core_vizs.media_type_donut_chart.first.auto_html_json
@@ -60,7 +46,6 @@ class Impl::DataProviders::ReportBuilder
     reusable_content_value = core_vizs.reusable.first.auto_html_json
     reusable_content_value["title"] = "Open for Re-use"
     variable_object[reusable_content_key] = reusable_content_value
-
 
     return variable_object, core_template
   end
