a790a43f1c342f6c3d1b7ec4681aab92932ffcdf
amitbadheka <ab@pykih.com>
2016-05-30 15:21:56 +0530

    Modifies simplecov to ignore coverage for views and mailers

--- .simplecov
+++ .simplecov
@@ -9,10 +9,11 @@ SimpleCov.start do
   add_filter '/config/'
   add_filter '/lib/'
   add_filter '/vendor/'
+  add_filter '/app/views'
+  add_filter '/app/mailers'
+  add_filter '/app/assets'
 
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
   add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Views', 'app/views'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,12 +27,14 @@ SimpleCov.start do
   add_filter '/config/'
   add_filter '/lib/'
   add_filter '/vendor/'
+  add_filter '/app/views'
+  add_filter '/app/mailers'
+  add_filter '/app/assets'
+
 
   add_group 'Controllers', 'app/controllers'
   add_group 'Models', 'app/models'
   add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Views', 'app/views'
 end
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
