2020fb7ba88042aa786ad91ddaa0d5725589ba55
Richard Doe <richard.doe@rwdit.net>
2016-10-04 13:43:38 +0100

    Simplify Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -2,29 +2,11 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-app_dir = File.expand_path("../..", __FILE__)
-
 preload_app!
 
-rackup DefaultRackup
-rails_env = ENV['RACK_ENV'] || 'development'
-
-if rails_env == 'development'
-  port ENV['PORT'] || 3000 if rails_env == 'development'
-else
-  bind "unix://#{app_dir}/tmp/sockets/puma.sock"
-end
-
-environment rails_env
-
-# Logging
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
-
-# Set master PID and state locations
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
-activate_control_app
-
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
