8f261f574a13cc65cd6a171d6e0ab60c2bdd85d9
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-11-15 17:45:30 +0100

    only run scheduled stats update at begining of month

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,11 @@ require 'clockwork'
 
 include Clockwork
 
-every(1.month, 'stats.update') do
-  Europeana::StatisticsDashboard::Application.load_tasks
-  Rake::Task['scheduled_jobs:load'].invoke
+every(1.day, 'stats.update', at: '03:30') do
+  # This is only ran on the first day of the month
+  if Date.today == Date.today.at_beginning_of_month
+    puts "it's today!"
+    Europeana::StatisticsDashboard::Application.load_tasks
+    Rake::Task['scheduled_jobs:load'].invoke
+  end
 end
