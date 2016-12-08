25922d7e351b1738baa444a686ad2ac511677420
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-12-08 10:40:13 +0100

    stop running aggregations again and again

--- app/workers/impl/data_providers/traffic_builder.rb
+++ app/workers/impl/data_providers/traffic_builder.rb
@@ -17,12 +17,15 @@ class Impl::DataProviders::TrafficBuilder
       data_provider.impl_report.delete if data_provider.impl_report.present?
       return nil
     end
+    ga_start_date = data_provider.last_updated_at.present? ? (data_provider.last_updated_at + 1).strftime('%Y-%m-%d') : '2012-01-01'
+    ga_end_date   = (Date.today.at_beginning_of_month - 1).strftime('%Y-%m-%d')
+
+    return unless ga_start_date < ga_end_date
+
     data_provider.update_attributes(status: 'Building Pageviews', error_messages: nil)
     data_provider_pageviews_output = Impl::Output.find_or_create(data_provider_id, 'Impl::Aggregation', 'pageviews')
     data_provider_visits_output = Impl::Output.find_or_create(data_provider_id, 'Impl::Aggregation', 'visits')
     data_provider_pageviews_output.update_attributes(status: 'Building Pageviews', error_messages: nil)
-    ga_start_date = data_provider.last_updated_at.present? ? (data_provider.last_updated_at + 1).strftime('%Y-%m-%d') : '2012-01-01'
-    ga_end_date   = (Date.today.at_beginning_of_month - 1).strftime('%Y-%m-%d')
 
     ga_dimensions = 'ga:month,ga:year'
     page_view_metrics = 'ga:pageviews'
