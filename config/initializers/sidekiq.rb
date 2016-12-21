e705e5f7b0014766d97a5f3e76257adb3201789a
Lutz Biedinger <lutz.biedinger@gmail.com>
2016-12-21 15:02:02 +0100

    allow setting redis url via env var

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,7 @@ redis_url = nil
 if Rails.application.config.cache_store[0] == :redis_store && Rails.application.config.cache_store[1]
   redis_url = Rails.application.config.cache_store[1]
 end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url, namespace: 'sidekiq' }
 end
